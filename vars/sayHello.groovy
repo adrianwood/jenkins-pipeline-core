a9430bb1c9bba338ce32547a584373aeed0cbb78
Adrian Wood <adrianwood30@gmail.com>
2017-02-27 22:21:32 +0000

    AAdded

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,4 +2,22 @@ def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     echo "Hello, ${name}."
+    stages {
+        stage('Build') { 
+            steps { 
+                sh 'make' 
+            }
+        }
+        stage('Test'){
+            steps {
+                sh 'make check'
+                junit 'reports/**/*.xml' 
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh 'make publish'
+            }
+        }
+    }
 }
