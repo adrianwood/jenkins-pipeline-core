0b6a4e2bef9f95fcfc419a4c6fe103ac60398d26
Adrian Wood <adrianwood30@gmail.com>
2017-02-27 18:39:21 +0000

    AAdded

--- src/org/foo/buildPlugin.groovy
+++ src/org/foo/buildPlugin.groovy
@@ -1,13 +1,1 @@
-// vars/acme.groovy
-class acme implements Serializable {
-    private String name
-    def setName(value) {
-        name = value
-    }
-    def getName() {
-        name
-    }
-    def caution(message) {
-        echo "Hello, ${name}! CAUTION: ${message}"
-    }
-}
+echo 'test'
