d27883c541045dfb553af1ca89d93f1acd2c60e6
Adrian Wood <adrianwood30@gmail.com>
2017-02-27 17:51:55 +0000

    Add

--- src/org/foo/Bar.groovy
+++ src/org/foo/Bar.groovy
@@ -1,16 +1,15 @@
+// vars/buildPlugin.groovy
 def call(body) {
+    // evaluate the body block, and collect configuration into the object
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    stage 'checkout'
+
+    // now build, based on the configuration provided
     node {
-        checkout scm
-        stage 'main'
-        docker.image(config.environment).inside {
-            sh config.mainScript
-        }
-        stage 'post'
-        sh config.postScript
+        git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
+        sh "mvn install"
+        mail to: "...", subject: "${config.name} plugin build", body: "..."
     }
 }
