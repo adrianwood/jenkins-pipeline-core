b48f6c140525733f16c860984d280cc282f88989
Adrian Wood <adrianwood30@gmail.com>
2017-02-27 22:35:59 +0000

    AAdded

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,4 +2,15 @@ def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     echo "Hello, ${name}."
+    node {
+    // This is the current syntax for invoking a build wrapper, naming the class.
+        // Just some echoes to show the timestamps.
+        stage "First echo"
+        echo "Hey, look, I'm echoing with a timestamp!"
+
+        // And a final echo to show the time when we wrap up.
+        stage "Second echo"
+        echo "Wonder what time it is now?"
 }
+}
+
