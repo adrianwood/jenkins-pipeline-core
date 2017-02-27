343222b861e65d96469909a8cef5afdf3137a54f
Adrian Wood <adrianwood30@gmail.com>
2017-02-27 23:07:21 +0000

    AAdded

--- vars/preBuild.groovy
+++ vars/preBuild.groovy
@@ -2,6 +2,7 @@ def call(String name = 'human') {
     node {
         stage "Checking out"
         echo "Hey, look, I'm echoing with a timestamp!"
+	git url: 'https://github.com/jfrogdev/project-examples.git'
 
         stage "Building"
         echo "Wonder what time it is now?"
